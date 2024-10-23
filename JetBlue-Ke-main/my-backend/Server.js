58b16cfa621cedf8f8c5611d96bea9d68c0d7d3e
smith-maina7 <smithmaina7@gmail.com>
2024-10-23 10:56:40 -0600

    WORKING ON BACKEND

--- JetBlue-Ke-main/my-backend/Server.js
+++ JetBlue-Ke-main/my-backend/Server.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import axios from 'axios'
 import dotenv from 'dotenv'
+import axios from 'axios'
 
 dotenv.config()
 
@@ -15,21 +15,22 @@ const tenantId = process.env.TENANT_ID
 const authority = `https://login.microsoftonline.com/${tenantId}`
 const tokenEndpoint = `${authority}/oauth2/v2.0/token`
 
-async function getAccessToken() {
-  const formData = new URLSearchParams({
-    client_id: clientId,
-    client_secret: clientSecret,
-    scope: 'https://graph.microsoft.com/.default',
-    grant_type: 'client_credentials',
-  })
-
+app.post('/get-token', async (req, res) => {
   try {
-    const response = await axios.post(tokenEndpoint, formData.toString(), {
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    const response = await axios.post(tokenEndpoint, null, {
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        scope: 'https://graph.microsoft.com/.default',
+        grant_type: 'client_credentials',
+      },
     })
-    return response.data.access_token
+    res.json(response.data)
   } catch (error) {
-    console.error('Error fetching access token:', error)
-    throw new Error('Unable to retrieve access token')
+    res.status(500).json({ error: error.message })
   }
-}
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})

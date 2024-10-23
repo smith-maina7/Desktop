87628643bd6ee8125b3f00f4db509361f095193b
smith-maina7 <smithmaina7@gmail.com>
2024-10-23 09:59:27 -0600

    Initialized the project

---
+++ JetBlue-Ke-main/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+  </nav>
+
+
+</template>
+
+<style scoped></style>
